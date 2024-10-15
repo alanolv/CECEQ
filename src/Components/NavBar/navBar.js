8f2da824b27d0a3c56687e480ee982327310f98c
alanolv <olvera.cristino.alan@gmail.com>
2024-10-14 22:26:59 -0600

    feat: end navbar

--- src/Components/NavBar/navBar.js
+++ src/Components/NavBar/navBar.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React  from "react";
 import "./navBar.css";
 import { NavLink } from "react-router-dom";
 import logoM from "../../sources/images/logo_gm.png";
 import heraldica from "../../sources/images/heraldica.png";
+import arrowLineUp from "../../sources/svg/arrowLineUp.svg";
+import arrowLineDown from "../../sources/svg/arrowLineDown.svg";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [hoveredLink, setHoveredLink] = useState(null);
+
+  const handleMouseEnter = (link) => {
+    setHoveredLink(link);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredLink(null);
+  };
   return (
     <div className="navbar-content">
       <div className="navbar-logos">
         <img src={logoM} alt="logo"/>
         <img src={heraldica} alt="logo"/>
       </div>
-      <div className="navbar-links">
-        <NavLink to="/" className="nav-link">
-          Inicio
-          <div className="navbar-dropdown" style={{left:"-110%"}}>
+      <div className="navbar-links" >
+        <NavLink to="/" className="nav-link" onMouseEnter={() => handleMouseEnter("inicio")} onMouseLeave={handleMouseLeave}>
+          Inicio <img src={ hoveredLink === "inicio" ? arrowLineDown : arrowLineUp} alt="arrow" width={10}/>
+          <div className="navbar-dropdown" style={{left:"-82%"}}>
             <NavLink to="/Bienvenida">Bienvenida</NavLink>
             <NavLink to="/Directorio">Directorio</NavLink>
             <NavLink to="/HorarioAtención">Horario de atención</NavLink>
@@ -22,9 +34,9 @@ export default function NavBar() {
             <NavLink to="/Nosotros">Nosotros</NavLink>
           </div>
         </NavLink>
-        <NavLink to="/Servicios" className="nav-link" >
-          Servicios
-          <div className="navbar-dropdown" style={{left:"-55%"}}>
+        <NavLink to="/Servicios" className="nav-link" onMouseEnter={() => handleMouseEnter("servicio")} onMouseLeave={handleMouseLeave}>
+          Servicios <img src={ hoveredLink === "servicio" ? arrowLineDown : arrowLineUp} alt="arrow" width={10}/>
+          <div className="navbar-dropdown" style={{left:"-40%"}}>
             <NavLink to="/Biblioteca">Biblioteca publica</NavLink>
             <NavLink to="/CentroComputo">Centro de computo</NavLink>
             <NavLink to="/Talleres">Talleres</NavLink>
@@ -33,9 +45,9 @@ export default function NavBar() {
             <NavLink to="/Visitas">Visitas</NavLink>
           </div>
         </NavLink>
-        <NavLink to="/Comunicación" className="nav-link">
-          Comunicación
-          <div className="navbar-dropdown" style={{left:"-10%"}}>
+        <NavLink to="/Comunicación" className="nav-link" onMouseEnter={() => handleMouseEnter("comunicacion")} onMouseLeave={handleMouseLeave}>
+          Comunicación <img src={ hoveredLink === "comunicacion" ? arrowLineDown : arrowLineUp} alt="arrow" width={10}/>
+          <div className="navbar-dropdown" style={{left:"-5%"}}>
             <NavLink to="/Boletines">Boletines</NavLink>
             <NavLink to="/Tríptico">Tríptico</NavLink>
             <NavLink to="/Ejeradio">Eje 5 radio</NavLink>
