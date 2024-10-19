fde800922cf4e204763762b3cf1e801244609c73
alanolv <olvera.cristino.alan@gmail.com>
2024-10-19 00:03:58 -0600

    feat initialize footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -3,7 +3,13 @@ import "./footer.css";
 
 export default function Footer(){
     return(
-        <>
-        </>
+        <div className="footer">
+            <div className="footer-content">
+                <div className="footer-contact">
+                    <h3>Contacto</h3>
+                    <p>Correo: </p>
+                    </div>
+                    </div>
+        </div>
     )
 }
