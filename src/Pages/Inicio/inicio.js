a5d09f2140b6522aec62ccd0a15adc77f35481fd
alanolv <olvera.cristino.alan@gmail.com>
2024-11-06 14:42:27 -0600

    delete talleres

--- src/Pages/Inicio/inicio.js
+++ src/Pages/Inicio/inicio.js
@@ -14,21 +14,8 @@ export default function Inicio() {
           A continuacion encontraras algunos de muchos talleres que puedes en
           esta institucion
         </p>
-        <div className="inicio-talleres">
-          <figure class="tarjetas">
-            <div class="tarjetas__contenido">
-              <div class="tarjetas__elemento">
-              <img src="http://gomezmorin.queretaro.gob.mx/images/domingo235x377.jpg?crc=4270845572" alt="taller 1"/>
-              </div>
-              <div class="tarjetas__elemento">
-              <img src="http://gomezmorin.queretaro.gob.mx/images/solidwork232x373.jpg?crc=4117728260"  alt="taller 2"/>
-              </div>
-              <div class="tarjetas__elemento">
-              <img src="http://gomezmorin.queretaro.gob.mx/images/ciclo%20de%20cine235x377.jpg?crc=3894128011"  alt="taller 3"/>
-              </div>
-            </div>
-          </figure>
-        </div>
+        
+        
       </div>
     </div>
   );
