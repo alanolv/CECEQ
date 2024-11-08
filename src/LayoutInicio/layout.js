5ead2bf62922fc89f6a65d0569861507435a697f
alanolv <olvera.cristino.alan@gmail.com>
2024-11-08 14:13:45 -0600

    fix: fix layout logic

--- src/LayoutInicio/layout.js
+++ src/LayoutInicio/layout.js
@@ -4,25 +4,29 @@ import Footer from "../Components/Footer/footer";
 import "./layout.css";
 import { useLocation } from "react-router-dom";
 
-export default function Layout({children}){{
+export default function Layout({ children }) {
+  {
     const location = useLocation();
 
-    return(
-        <div className="content-layout">
-        {location.pathname !== "/" && (
-
-        <>
+    return (
+      <div className="content-layout">
         <div className="navbars">
-        <NavBar/>
+          <NavBar />
         </div>
         <div className="content">
-        {children}
+          {location.pathname !== "/" && (
+            <>
+              <div clasname="banner"></div>
+              <div className="social-sidebar"></div>
+              {children}
+            </>
+          )}
+          {children}
         </div>
         <div className="footer">
-        <Footer/>
+          <Footer />
         </div>
-        </>
-        )}
-        </div>
-    )
-}}
+      </div>
+    );
+  }
+}
