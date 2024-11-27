0389525e3a72ccccbc6c3cda0d85450a59b0ac35
alanolv <olvera.cristino.alan@gmail.com>
2024-11-27 15:33:55 -0600

    feat new content page added to instalaciones

--- src/Pages/SeccionServicio/Instalaciones/instalaciones.js
+++ src/Pages/SeccionServicio/Instalaciones/instalaciones.js
@@ -31,6 +31,84 @@ export default function Instalaciones() {
           Requisitos para realizar escrito de petición de espacio(s) en el
           CECEQ:
         </p>
+        <ul>
+          Redactar oficio / escrito dirigido a la Mtra. ANA EUGENIA PATIÑO
+          CORREA, Directora General del Centro Educativo y Cultural del Estado
+          de Querétaro “Manuel Gómez Morin”.
+        </ul>
+        <p className="instalaciones-texto-marcado">
+          Con la siguiente información:
+        </p>
+        <ol>
+          <li>
+            Nombre del evento, fecha y horarios, costo por sesión, indicando las
+            edades de las personas a quienes está dirigido el evento.
+          </li>
+          <li>
+            Número de personas que asistirán al evento y las especificaciones
+            técnicas requeridas (mesas, sillas, sonido, proyector y micrófonos).
+          </li>
+          <li>
+            Contenido y temario por sesión del curso, taller, conferencia, etc.
+          </li>
+          <li>
+            Nombre, teléfono, correo electrónico del responsable del evento.
+          </li>
+          <li>
+            Curriculum Vitae, copia de título profesional, constancias, copia de
+            la credencial de elector o pasaporte, copia de comprobante de
+            domicilio vigente del responsable del evento.
+          </li>
+          <li>Catálogo, en caso de ser exposición artística.</li>
+          <li>
+            Firma al calce de la solicitud /oficio / escrito por parte del
+            responsable del evento.
+          </li>
+        </ol>
+        <p className="instalaciones-texto-marcado">
+          Importante considerar que:
+        </p>
+        <ol type="I">
+        <li>
+          El espacio deberá ser solicitado única y exclusivamente por la persona
+          interesada y calificada para dar el curso, taller, conferencia, etc.
+          No se aceptarán solicitudes realizadas a nombre de terceros.
+        </li>
+        <li>
+          Toda solicitud deberá presentarse con mínimo 3 semanas de anticipación
+          a la fecha planeada para el evento.
+        </li>
+        <li>
+          El horario de recepción de documentos de lunes a viernes de 09:00 a
+          16:00 h en la recepción de Dirección General, ubicada en el acceso D
+          planta alta.
+        </li>
+        <li>
+          La respuesta se emite 8 días hábiles posteriores a la recepción de la
+          solicitud, ésta puede ser positiva o negativa, y será entregada de
+          lunes a viernes de 09:00 a 16:00 h en la recepción de Dirección
+          General, ubicada en el acceso D planta alta.
+        </li>
+        </ol>
+        <p className="instalaciones-texto-marcado">
+          En caso de que la solicitud obtenga respuesta positiva para el uso y
+          disfrute de los espacios públicos del CECEQ:
+        </p>
+        <ol type="a">
+          <li>
+            Se deberán observar rigurosamente los lineamientos correspondientes
+          </li>
+          <li>
+            Se deberá enviar una propuesta de cartel promocional en un plazo no
+            mayor a 5 días posteriores a la entrega de respuesta, para que el
+            Área de Diseño del CECEQ lo apruebe.
+          </li>
+        </ol>
+        <p>
+          En caso de cancelación para el uso y disfrute de los espacios públicos
+          del CECEQ, el aviso deberá realizarse con 7 días hábiles de
+          anticipación.
+        </p>
       </div>
     </div>
   );
