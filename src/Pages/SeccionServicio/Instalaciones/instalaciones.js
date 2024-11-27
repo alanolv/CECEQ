a32325e948e5677c5c151d86a5827c4c7039ba72
alanolv <olvera.cristino.alan@gmail.com>
2024-11-27 15:16:02 -0600

    fix logo navbar issue

--- src/Pages/SeccionServicio/Instalaciones/instalaciones.js
+++ src/Pages/SeccionServicio/Instalaciones/instalaciones.js
@@ -1,9 +1,37 @@
 import React from "react";
+import "./instalaciones.css";
 
 export default function Instalaciones() {
   return (
-    <div>
+    <div className="instalaciones">
       <h2>Instalaciones</h2>
+      <div className="instalaciones-texto">
+        <p>
+          Uno de los servicios más importantes que ofrece el Centro Educativo y
+          Cultural del Estado de Querétaro (CECEQ) “Manuel Gómez Morin”, es el
+          préstamo de espacios para realizar actividades académicas, culturales,
+          educativas, sociales, recreativas y de divulgación, en un área de
+          20,500 metros cuadrados que se compone por el Patio Central que tiene
+          1,822 metros cuadrados; el Auditorio para 250 personas; 7 Salones que
+          van desde 64 hasta 128 metros cuadrados y, 18 Aulas con cupos que van
+          de 20 hasta 120 personas.
+        </p>
+        <p>
+          Un valor agregado a estas facilidades es su ubicación privilegiada en
+          una de las esquinas más importantes de la Ciudad de Santiago de
+          Querétaro, Qro., justo enfrente del pulmón natural de esta urbe que es
+          la Alameda “Miguel Hidalgo”.
+        </p>
+        <p>
+          La mayor parte de las instalaciones están equipadas con aire
+          acondicionado y equipo audiovisual (cañón, computadora y pantalla)
+          necesarios para la realización de cualquier evento.
+        </p>
+        <p className="instalaciones-texto-marcado">
+          Requisitos para realizar escrito de petición de espacio(s) en el
+          CECEQ:
+        </p>
+      </div>
     </div>
   );
 }
