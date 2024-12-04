c6aae1e65c1f084cadc9d2b4af0acce4555ea9ee
alanolv <olvera.cristino.alan@gmail.com>
2024-12-03 18:13:29 -0600

    feat add page content to marcoJuridico

--- src/Pages/SeccionInicio/HorarioDeAtencion/horarioAtencion.js
+++ src/Pages/SeccionInicio/HorarioDeAtencion/horarioAtencion.js
@@ -1,9 +1,90 @@
 import React from "react";
+import "./horarioAtencion.css";
 
 export default function HorarioAtencion() {
   return (
-  <>
-  <h2>Horario de Atencion</h2>
-  </>
+    <div className="atencion">
+      <h2>Horarios de Atencion</h2>
+      <div className="atencion-tabla">
+        {/* Horarios de atencion */}
+        <div className="directorio-tabla">
+          <div className="atencion-tabla-titulo">
+            <p>HORARIO</p>
+          </div>
+          <div className="directorio-tabla-contenido">
+            <div className="directorio-tabla-contenido-uno">
+              <p>Horario Administrativo</p>
+              <p>Lunes a viernes:</p>
+              <p>08:30 a 16:00 hrs.</p>
+              <p>Teléfono:</p>
+              <p>Conmutador (442) 251 9600</p>
+              <p>Extensión: 9613</p>
+            </div>
+            <div className="directorio-tabla-contenido-dos">
+              <p>
+                Horario de la Biblioteca Pública Central Estatal Francisco
+                Cervantes
+              </p>
+              <p>Martes a sábado:</p>
+              <p>09:00 a 17:00 hrs.</p>
+              <p>Teléfono:</p>
+              <p>Conmutador (442) 251 9600</p>
+              <p>Extensión: 9708</p>
+            </div>
+          </div>
+
+          <div className="directorio-tabla-contenido">
+            <div className="directorio-tabla-contenido-uno">
+              <p>
+                Horario del Área Infantil de la Biblioteca Pública Central
+                Estatal
+              </p>
+              <p>Martes a sábado:</p>
+              <p>09:00 a 17:00 hrs.</p>
+              <p>Teléfono:</p>
+              <p>Conmutador (442) 251 9600</p>
+              <p>Extensión: 9727</p>
+            </div>
+            <div className="directorio-tabla-contenido-dos">
+              <p>Horario del Centro de Cómputo</p>
+              <p>Martes a viernes:</p>
+              <p>09:00 a 17:30 hrs.</p>
+              <p>Sábado:</p>
+              <p>09:00 a 15:00 hrs.</p>
+              <p>Teléfono:</p>
+              <p>Conmutador (442) 251 9600</p>
+              <p>Extensión: 9722</p>
+            </div>
+          </div>
+
+          <div className="directorio-tabla-contenido">
+            <div className="directorio-tabla-contenido-uno">
+              <p>Horario de la Librería “EDUCAL”</p>
+              <p>Lunes a viernes:</p>
+              <p>08:30 a 16:00 hrs.</p>
+              <p>Teléfono:</p>
+              <p>Conmutador -</p>
+              <p>Extensión: 9720</p>
+            </div>
+            <div className="directorio-tabla-contenido-dos">
+              <p>Horario del INEGI</p>
+              <p>Lunes a Viernes:</p>
+              <p>08:30 a 16:00 hrs.</p>
+              <p>Teléfono:</p>
+              <p>Conmutador -</p>
+              <p>Extensión: 9718</p>
+            </div>
+          </div>
+          <div className="directorio-tabla-contenido-principal">
+            <p>Horario de PREPA SI</p>
+            <p>Lunes a Viernes:</p>
+            <p>08:30 a 16:00 hrs.</p>
+            <p>Teléfono:</p>
+            <p>Conmutador -</p>
+            <p>Extensión: 9729</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/Pages/SeccionInicio/MarcoJuridico/marcoJuridico.js
+++ src/Pages/SeccionInicio/MarcoJuridico/marcoJuridico.js
@@ -1,9 +1,53 @@
 import React from "react";
+import "./marcoJuridico.css";
+import { NavLink } from "react-router-dom";
 
 export default function MarcoJuridico() {
   return (
-    <>
-      <h2>Marco Juridico</h2>
-    </>
+    <div className="marco">
+      <h2>MARCO JURÍDICO</h2>
+      <div className="atencion-tabla">
+        {/* Horarios de atencion */}
+        <div className="directorio-tabla">
+          <div className="atencion-tabla-titulo">
+            <p>Leyes y Decretos</p>
+          </div>
+          <div className="directorio-tabla-contenido">
+            <div className="directorio-tabla-contenido-uno">
+              <NavLink to="http://gomezmorin.queretaro.gob.mx/assets/lgb.pdf"className="marco-link">
+              <p>Ley General de Bibliotecas.</p>
+              </NavLink>
+            </div>
+            <div className="directorio-tabla-contenido-dos">
+            <NavLink to="http://gomezmorin.queretaro.gob.mx/assets/primerdecretocreacion.pdf"className="marco-link">
+              <p>
+                Decreto por el que se crea el Centro Educativo y Cultural del
+                Estado de Querétaro
+              </p>
+              </NavLink>
+            </div>
+          </div>
+          <div className="directorio-tabla-contenido">
+            <div className="directorio-tabla-contenido-uno">
+            <NavLink to="http://gomezmorin.queretaro.gob.mx/assets/segundodecretocreacion.pdf"className="marco-link">
+              <p>
+                Decreto que reforma y deroga diversas disposiciones del Decreto
+                por el que se crea el Centro Educativo y Cultural del Estado de
+                Querétaro (CECEQ) “Manuel Gómez Morín”
+              </p>
+              </NavLink>
+            </div>
+            <div className="directorio-tabla-contenido-dos">
+            <NavLink to="http://gomezmorin.queretaro.gob.mx/assets/reglamento-interior.pdf" className="marco-link">
+              <p>
+                Reglamento interior del Centro Educativo y Cultural del Estado
+                de Querétaro (CECEQ) “Manuel Gómez Morín”
+              </p>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
