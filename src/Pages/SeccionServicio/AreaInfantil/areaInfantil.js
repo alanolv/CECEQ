c32ccd81c4544ca05c57bf0ed5112df23923c117
alanolv <olvera.cristino.alan@gmail.com>
2024-12-15 22:37:46 -0600

    feat add page content to area infantil

--- src/Pages/SeccionServicio/AreaInfantil/areaInfantil.js
+++ src/Pages/SeccionServicio/AreaInfantil/areaInfantil.js
@@ -1,9 +1,168 @@
 import React from "react";
+import "./areaInfantil.css";
 
 export default function AreaInfantil() {
   return (
-    <div>
-      <h2>Area Infantil</h2>
+    <div className="area-infantil">
+      <h2>ÁREA INFANTIL</h2>
+      <div className="visitas-texto">
+        <p className="visitas-texto-marcado">SERVICIOS</p>
+        <p>
+          Por ser un área más de la Biblioteca Pública Central Estatal Francisco
+          Cervantes, los servicios que se ofrecen a los usuarios son: los
+          préstamos internos con estantería abierta y a domicilio, éstos últimos
+          únicamente para libros de la colección general; orientación a quienes
+          nos visitan con el fin de dar a conocer la gama de actividades y
+          opciones que se ofrecen en este Centro Educativo y Cultural del Estado
+          de Querétaro.
+        </p>
+        <p className="visitas-texto-marcado">INSCRIPCIÓN</p>
+        <ol>
+          <li>
+            Acudir a la recepción de Biblioteca Infantil y proporcionar los
+            datos generales del niño o niña para su registro: Nombre completo,
+            fecha de nacimiento, domicilio, teléfono de contacto.
+          </li>
+        </ol>
+        <p className="visitas-texto-marcado">
+          REQUISITOS PARA TRAMITAR CREDENCIAL
+        </p>
+        <p>
+          Importante: Todos los documentos deberán presentarse en original y
+          copia
+        </p>
+        <ol>
+          <li>
+            Antes de realizar el trámite el niño deberá tener su número de
+            usuario (registro en el área Infantil, se obtiene con la segunda
+            visita).
+          </li>
+          <li>
+            <strong>El usuario (0 a 12 años) </strong>deberá presentarse para el
+            inicio del trámite (aquí le tomamos la foto).
+          </li>
+          <li>
+            Se entrega la credencial, cuatro días hábiles después del trámite y
+            se presenta su aval para firmar los documentos.
+          </li>
+          <li>
+            <strong>El usuario (0 a 12 años) </strong>presenta y entrega los
+            documentos siguientes:
+            <ol type="a">
+              <li>
+                Credencial de Identificación oficial: Escolar, seguro social
+                (con foto) o pasaporte.
+              </li>
+              <li>Acta de nacimiento o CURP</li>
+              <li>
+                Datos de la escuela (nombre, calle, número, colonia, teléfono)
+              </li>
+            </ol>
+          </li>
+
+          <li>
+            <strong> El aval o fiador mayor de 18 años </strong> y que trabaje
+            en: empresa, institución o negocio propio (comprobable), debe
+            presentar:
+            <ol type="a">
+              <li>
+                Identificación oficial vigente: únicamente credencial de
+                elector, Instituto Nacional de las Personas Adultas Mayores
+                (INAPAM), pasaporte o licencia para conducir (registrado en la
+                ciudad de Querétaro o su área metropolitana).
+              </li>
+              <li>
+                Datos del trabajo: Nombre de la empresa o institución,
+                ocupación, dirección, número de teléfono-
+              </li>
+              <li>
+                Comprobante de domicilio: Luz, agua, gas, predial, teléfono.
+              </li>
+              <li>Podrá ser aval de un máximo de 4 usuarios.</li>
+            </ol>
+          </li>
+        </ol>
+        <p className="visitas-texto-marcado">
+          COLECCIONES DE LA BIBLIOTECA INFANTIL
+        </p>
+        <p>
+          Contiene materiales diversos destinados a los niños integrada por:
+          libros de estudio, obras de carácter general, de consulta
+          (diccionarios, enciclopedias, atlas, etc.) y libros recreativos.
+        </p>
+        <ol>
+          <li>
+            <strong>Colección General:</strong> Constituida por libros de temas
+            específicos (literatura, matemáticas, física, química, historia,
+            arte, astronomía, arte, viajes, geografía, etc.), dirigidos a
+            fomentar la lectura recreativa e informativa (incluye la colección
+            de Bebé…teca).
+          </li>
+          <li>
+            <strong>Colección de Consulta:</strong> Compuesta por diccionarios,
+            enciclopedias, directorios, atlas, almanaques, etc. Estas obras no
+            fueron elaboradas para leerse de principio a fin en una sola
+            lectura, sino que resuelven las dudas específicas, por su naturaleza
+            no se prestan a domicilio.
+          </li>
+          <li>
+            <strong>Colección de Material Audiovisual:</strong> Es el conjunto
+            de documentales con temáticas históricas, científicas y películas
+            recreativas.
+          </li>
+          <li>
+            <strong>Libros Interactivos: </strong>Este material bibliográfico
+            tiene características especiales que ayudan a los infantes en su
+            formación y recreación, contando con libros tridimensionales,
+            páginas plegables, rompecabezas, entre otros. Por la utilización y
+            características del material recreativo, su uso es exclusivo en las
+            instalaciones de la Sala de Lectura.
+          </li>
+          <li>
+            <strong>Material Lúdico: </strong>Como un área de recreación para
+            chicos y grandes, en el interior de la biblioteca existe un espacio
+            destinado a los juegos de mesa y de piso, por ejemplo: rompecabezas
+            de cartón y de madera, turista mundial, palillos y damas chinas,
+            lotería, oca, dominó, ajedrez, entre otros; en donde la pericia, la
+            imaginación y la convivencia harán de tu día una experiencia
+            inolvidable.
+          </li>
+        </ol>
+        <p className="visitas-texto-marcado">
+          IMPORTANCIA DE LA LECTURA DESDE LA PRIMERA INFANCIA
+        </p>
+        <p>
+          “La lectura en la primera infancia no se limita, por lo tanto, a ser
+          simplemente una herramienta para poner en marcha competencias básicas
+          que definan el éxito alfabético o la productividad adulta, pues su
+          máximo potencial está ligado al desarrollo emocional y a los vínculos
+          relacionales profundos que se establecen entre madre e hijo y que se
+          extienden a todo el ámbito familiar.
+        </p>
+        <p>
+          La revelación de que las palabras nos descifran, nos “cuentan” y nos
+          construyen por dentro conecta la experiencia de la lectura, más allá
+          de su función instrumental, con la vida y con el afecto y esta
+          experiencia se constituye en sustrato para enriquecer la comunicación
+          en el ámbito familiar y para promover actitudes de buen trato, lo cual
+          le confiere un enorme poder preventivo desde el punto de vista
+          emocional. Así mismo, por estar inmersa en plano afectivo y alejada de
+          las presiones académicas, la experiencia de leer en la primera
+          infancia puede vincularse al placer -no al deber ni al éxito o fracaso
+          escolar-. En este sentido, se ha demostrado que un acercamiento
+          temprano a los libros y a las diversas posibilidades de comunicación
+          humanas presentes en la música, en el arte y en el lenguaje oral
+          revierte en una motivación perdurable que impulsa a los niños a amar
+          la lectura y a querer seguir leyendo a lo largo de su vida. En última
+          instancia, este sencillo argumento justifica la importancia de leer
+          cuentos, cantar y jugar con las palabras en la niñez para garantizar
+          hábitos lectores duraderos que acompañarán al ser humano durante su
+          proceso formativo a lo largo de la vida”.
+        </p>
+        <div className="area-infantil-autor">
+          <p>- Yolanda Reyes</p>
+        </div>
+      </div>
     </div>
   );
 }
