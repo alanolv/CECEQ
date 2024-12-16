ff484e910e5bb9e58171bd2f1318e6f87a1a71bd
alanolv <olvera.cristino.alan@gmail.com>
2024-12-16 01:32:28 -0600

    feat add page content to galeria and murales pages

--- src/Pages/SeccionGalerias/Imagenes/imgGallery.js
+++ src/Pages/SeccionGalerias/Imagenes/imgGallery.js
@@ -1,11 +1,124 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
 import "./imgGallery.css";
-
+import "react-image-gallery/styles/css/image-gallery.css";
+import * as Galeria from "../../../sources/utilities/ExportGalerias/exportGalerias";
 
 export default function ImgGallery() {
-    return(
-        <div className="imgGallery">
-            <h2>Galerias</h2>
-        </div>
-    )};
+  const principalImages = [
+    {
+      original: Galeria.Galeria1,
+      thumbnail: Galeria.Galeria1,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria2,
+      thumbnail: Galeria.Galeria2,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria3,
+      thumbnail: Galeria.Galeria3,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria4,
+      thumbnail: Galeria.Galeria4,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria5,
+      thumbnail: Galeria.Galeria5,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria6,
+      thumbnail: Galeria.Galeria6,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria7,
+      thumbnail: Galeria.Galeria7,
+      originalHeight: 500,
+    },
+  ];
+  const [images, setImages] = React.useState(principalImages);
+
+  const handleLinkClick = (newImages) => {
+    setImages(newImages);
+  };
+
+  const titularCeceq = [
+    {
+      original: Galeria.Galeria13,
+      thumbnail: Galeria.Galeria13,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+  ];
+  const womenPower = [
+    {
+        original: Galeria.Galeria8,
+        thumbnail: Galeria.Galeria8,
+        originalHeight: 500,
+        originalWidth: 150,
+      },
+    {
+      original: Galeria.Galeria9,
+      thumbnail: Galeria.Galeria9,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria10,
+      thumbnail: Galeria.Galeria10,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria11,
+      thumbnail: Galeria.Galeria11,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+    {
+      original: Galeria.Galeria12,
+      thumbnail: Galeria.Galeria12,
+      originalHeight: 500,
+      originalWidth: 150,
+    },
+  ];
+  return (
+    <div className="imgGallery">
+      <h2>Galerias</h2>
+      <p
+        className="gallery-vinculo"
+        onClick={() => handleLinkClick(principalImages)}
+      >
+        PRESENTAN LIBRO “2030: NUEVOS PARADIGMAS EN LAS RELACIONES”.
+      </p>
+      <p
+        className="gallery-vinculo"
+        onClick={() => handleLinkClick(womenPower)}
+      >
+        EXPOSICIÓN WOMENPOPWER.
+      </p>
+      <p
+        className="gallery-vinculo"
+        onClick={() => handleLinkClick(titularCeceq)}
+      >
+        DESIGNAN AL TITULAR DEL CENTRO EDUCATIVO Y CULTURAL “MANUEL GÓMEZ
+        MORÍN”.
+      </p>
+      <div className="gallery">
+        <ImageGallery items={images} />
+      </div>
+    </div>
+  );
+}
--- src/Pages/SeccionGalerias/Murales/murales.js
+++ src/Pages/SeccionGalerias/Murales/murales.js
@@ -9,68 +9,101 @@ export default function Murales() {
     {
       original: Mural.Mural1,
       thumbnail: Mural.Mural1,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural2,
       thumbnail: Mural.Mural2,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural3,
       thumbnail: Mural.Mural3,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural4,
       thumbnail: Mural.Mural4,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural5,
       thumbnail: Mural.Mural5,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural6,
       thumbnail: Mural.Mural6,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural7,
       thumbnail: Mural.Mural7,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural8,
       thumbnail: Mural.Mural8,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural9,
       thumbnail: Mural.Mural9,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural10,
       thumbnail: Mural.Mural10,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural11,
       thumbnail: Mural.Mural11,
+
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural12,
       thumbnail: Mural.Mural12,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural13,
       thumbnail: Mural.Mural13,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural14,
       thumbnail: Mural.Mural14,
+      originalHeight: 500,
+      originalWidth: 150,
     },
     {
       original: Mural.Mural15,
       thumbnail: Mural.Mural15,
+      originalHeight: 500,
+      originalWidth: 150,
     },
   ];
   return (
     <div className="murales">
       <h2>Murales</h2>
-      <ImageGallery items={images} />
+      <div className="murales-images">
+        <ImageGallery items={images} />
+        </div>
     </div>
   );
 }
