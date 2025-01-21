415c2437ede8f80c519dddafc30359da606e51ee
alanolv <olvera.cristino.alan@gmail.com>
2025-01-21 12:29:40 -0600

    ref refactor carussel component logic

--- src/Components/Carousels/carousels.js
+++ src/Components/Carousels/carousels.js
@@ -1,30 +1,24 @@
 import './carousels.css';
-import BannerCadaTodos from "../../sources/images/CarruselImages/bannerCasaDeTodos.png"
-import Cultura360 from "../../sources/images/CarruselImages/cultura360.png"
-import eje5 from "../../sources/images/CarruselImages/eje5.png"
+import { Images } from "../../sources/utilities/ExportCarruselImages/exportCarruselImages"
 
 export default function Carrusel() {
   return (
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={BannerCadaTodos} class="d-block w-100 custom-image" alt="..."/>
+      <div class="carousel-inner">
+      {Images.map((image, index) => (
+          <div key={index} className={`carousel-item ${index === 0 ? "active" : ""}`}>
+            <img src={image.image} className="d-block w-100 custom-image"/>
+          </div>
+        ))}
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-item">
-      <img src={Cultura360} class="d-block w-100 custom-image" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={eje5} class="d-block w-100 custom-image" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
   );
 }
---
+++ src/sources/utilities/ExportCarruselImages/exportCarruselImages.js
@@ -0,0 +1,18 @@
+import CasaDeTodos from "../../images/CarruselImages/bannerCasaDeTodos.png"
+import Cultura360 from "../../images/CarruselImages/cultura360.png"
+import Eje5 from "../../images/CarruselImages/eje5.png"
+
+export const Images = [
+    {
+        image: CasaDeTodos,
+        path: "/#"
+    },
+    {
+        image: Cultura360,
+        path: "/#"
+    },
+    {
+        image: Eje5,
+        path: "/#"
+    }
+]
